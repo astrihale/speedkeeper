2f10f44514d441b5c01c8397a2169790abe9478c
astrihale <nanarietta@gmail.com>
2021-02-12 22:31:33 +0100

    Make xo happy

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 // Import the speed keeping logic
-const {start, stop, guard} = require('./speedkeeper.js');
+const {start, guard} = require('./speedkeeper.js');
 
 // Import only the necessary things from Electron to make a tray application
 const {app, Menu, Tray} = require('electron');
 const path = require('path');
-const assets = path.join(__dirname + '/assets');
+const assets = path.join(__dirname, '/assets');
 
 // The icon instance
 let icon = null;
 
 // Create the asynchronous routine for starting
 const routine = async () => {
-    // Wait for the application to be ready
-    await app.whenReady();
+	// Wait for the application to be ready
+	await app.whenReady();
 
-    // Hide it from the dock
-    app.dock.hide();
+	// Hide it from the dock
+	app.dock.hide();
 
-    // Find the icon that the application will show
-    icon = new Tray(path.join(assets, 'logo@3x.png'));
+	// Find the icon that the application will show
+	icon = new Tray(path.join(assets, 'logo@3x.png'));
 
-    // Set the tooltip
-    icon.setToolTip('speedkeeper');
-    icon.setToolTip('speedkeeper by Astrihale');
+	// Set the tooltip
+	icon.setToolTip('speedkeeper');
+	icon.setToolTip('speedkeeper by Astrihale');
 
-    // Create the context menu that will appear when you click the button
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Exit', type: 'normal', checked: true,
-            click: function (item, browserWindow) {
-                app.exit(0)
-            }
-        },
-        {type: 'separator'},
-        {label: 'Speedkeeper by Astrihale', type: "normal"}
-    ]);
-    start();
+	// Create the context menu that will appear when you click the button
+	const contextMenu = Menu.buildFromTemplate([
+		{
+			label: 'Exit', type: 'normal', checked: true,
+			click(_item, _browserWindow) {
+				app.exit(0);
+			}
+		},
+		{type: 'separator'},
+		{label: 'Speedkeeper by Astrihale', type: 'normal'}
+	]);
+	start();
 
-    // Show the context menu on tray
-    icon.setContextMenu(contextMenu);
+	// Show the context menu on tray
+	icon.setContextMenu(contextMenu);
 
-    // Set the listener for on and off
-    contextMenu.items[2].enabled = false;
+	// Set the listener for on and off
+	contextMenu.items[2].enabled = false;
 };
 
 // Start the asynchronous routine
--- speedkeeper.js
+++ speedkeeper.js
@@ -5,7 +5,7 @@ const cpuCores = require('os').cpus().length;
 // Create the speed keeping function
 function speedKeeper() {
     setInterval(() => {
-        1 + 1
+        1 + 1;
     }, 0.001);
 }
 
@@ -15,18 +15,20 @@ function start() {
     for (let i = 0; i < cpuCores; i++) {
         cluster.fork();
     }
+
     setTimeout(speedKeeper, 1);
 }
 
 // Create the guard function
 function guard(routine) {
-    if (cluster.isMaster)
+    if (cluster.isMaster) {
         setTimeout(routine, 1);
-    else
+    } else {
         speedKeeper();
+    }
 }
 
 // And export out the functions
 module.exports = {
     start, guard
-}
+};
